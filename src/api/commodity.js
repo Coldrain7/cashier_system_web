66151251f5dba6d53a0a0ce6ee5d9e696e8f6f96
Coldrain7 <2057787038@qq.com>
2024-01-29 01:21:35 +0800

    增加了修改商品的方法

--- src/api/commodity.js
+++ src/api/commodity.js
@@ -1,2 +1,3 @@
 import http from '@http/http'
 export const commodityPage = query => http.GET('./api/commodity/commodityPage', query)
+export const updateCommodity = commodity => http.GET('./api/commodity/updateCommodity', commodity)
