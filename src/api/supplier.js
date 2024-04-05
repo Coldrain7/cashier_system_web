bf1138e8ce5a4d53c774b417ccdc748a96a6ec83
Coldrain7 <2057787038@qq.com>
2024-04-06 00:52:58 +0800

    增加了供应商页面需要的api

--- src/api/supplier.js
+++ src/api/supplier.js
@@ -1,2 +1,6 @@
 import http from '@http/http'
 export const getSupplierOptions = supId => http.GET('./api/supplier/getOptions', supId)
+export const getSupplier = supplier => http.GET('./api/supplier/getSuppliers', supplier)
+export const getSupplierById = supplier => http.GET(`./api/supplier/${supplier.id}`, supplier)
+export const updateSupplier = supplier => http.GET('./api/supplier/updateSupplier', supplier)
+export const createSupplier = supplier => http.POST('./api/supplier/createSupplier', supplier)
