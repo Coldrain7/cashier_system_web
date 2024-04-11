ecd0cfc5d6dd431167f170c3b74d10a2a0c42f95
Coldrain7 <2057787038@qq.com>
2024-04-12 00:28:45 +0800

    增加了获取挂单的api

--- src/api/record.js
+++ src/api/record.js
@@ -1,4 +1,5 @@
 import http from '@http/http'
-export const sellCommodities = query => http.POST('./api/record/sellCommodities', query)
+export const createRecords = query => http.POST('./api/record/createRecords', query)
 export const getRecordsWithMember = query => http.GET('./api/record/getRecordsWithMember', query)
 export const getRecordWithCommodity = query => http.GET('./api/record/getRecordWithCommodity', query)
+export const getPendedRecords = query => http.GET(`./api/record/getPendedRecords/${query.supId}`, query)
