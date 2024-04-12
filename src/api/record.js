aa8d0759e2ef10f8eee656e55fb8431f73b8be63
Coldrain7 <2057787038@qq.com>
2024-04-13 01:43:21 +0800

    增加了获取挂单商品与根据id删除Record的api

--- src/api/record.js
+++ src/api/record.js
@@ -3,3 +3,5 @@ export const createRecords = query => http.POST('./api/record/createRecords', qu
 export const getRecordsWithMember = query => http.GET('./api/record/getRecordsWithMember', query)
 export const getRecordWithCommodity = query => http.GET('./api/record/getRecordWithCommodity', query)
 export const getPendedRecords = query => http.GET(`./api/record/getPendedRecords/${query.supId}`, query)
+export const getPendedCommodities = record => http.GET('./api/record/getPendedCommodities', record)
+export const deleteRecordById = record => http.DELETE(`./api/record/${record.id}`, record)
