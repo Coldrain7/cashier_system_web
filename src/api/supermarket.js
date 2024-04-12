143f2428d85e66ccacc2190092b368993942f862
Coldrain7 <2057787038@qq.com>
2024-04-13 01:42:31 +0800

    增加了获取店铺信息的api

--- src/api/supermarket.js
+++ src/api/supermarket.js
@@ -1,2 +1,3 @@
 import http from '@http/http'
 export const marketRegister = name => http.POST('./api/supermarket/register', name)
+export const getSupermarketById = query => http.GET(`./api/supermarket/${query.supId}`, query)
