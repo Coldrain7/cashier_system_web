24b6a2ba39a22fcdc3a040deb8ac30d8fd9c773a
Coldrain7 <2057787038@qq.com>
2024-04-18 00:35:15 +0800

    只增加了页面，没有功能

---
+++ src/pages/main/routes/event/Event.vue
@@ -0,0 +1,29 @@
+<template>
+  <div></div>
+</template>
+<script>
+import store from '../../../../store'
+import {getSupIdById} from '@api/user'
+export default {
+  data () {
+    return {
+      id: '',
+      query: {
+        supId: ''
+      }
+    }
+  },
+  methods: {
+
+  },
+  created () {
+    this.id = store.getters['user/getUserId']
+    getSupIdById({id: this.id}).then(res => {
+      this.query.supId = res.data
+    })
+  }
+}
+</script>
+<style scoped>
+
+</style>
